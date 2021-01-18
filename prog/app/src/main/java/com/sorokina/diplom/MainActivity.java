5d0e613379d2055b982e38e057d1b8a11f9d446a
Albina Sorokina <albinawolf@gmail.ru>
2021-01-18 17:55:23 +0700

    Linking to button Sport and Recipies

--- prog/app/src/main/java/com/sorokina/diplom/MainActivity.java
+++ prog/app/src/main/java/com/sorokina/diplom/MainActivity.java
@@ -11,12 +11,15 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button calor;
+    private Button sport;
+    private Button recipes;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         calor = (Button) findViewById(R.id.calories);
         calor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,11 +27,38 @@ public class MainActivity extends AppCompatActivity {
                 openCalories();
             }
         });
+
+        sport = (Button) findViewById(R.id.sport);
+        sport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSport();
+            }
+        });
+
+        recipes = (Button) findViewById(R.id.recipes);
+        recipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRecipes();
+            }
+        });
     }
+
+
 
     public void openCalories(){
         Intent intent_calor = new Intent(this, calories.class);
         startActivity(intent_calor);
     }
 
+    public void openSport(){
+        Intent intent_sport = new Intent(this, sport.class);
+        startActivity(intent_sport);
+    }
+
+    public void openRecipes(){
+        Intent intent_recipes = new Intent(this, recipes.class);
+        startActivity(intent_recipes);
+    }
 }
