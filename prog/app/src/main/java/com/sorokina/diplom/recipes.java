78e9571432edb75888db1b583f3b457dd5fc73fc
Albina Sorokina <albinawolf@gmail.ru>
2021-01-18 15:04:09 +0700

    Sketch of several activities

--- prog/app/src/main/java/com/sorokina/diplom/recipes.java
+++ prog/app/src/main/java/com/sorokina/diplom/recipes.java
@@ -2,16 +2,13 @@ package com.sorokina.diplom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class recipes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipes);
     }
-    
 }
