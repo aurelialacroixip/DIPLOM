d3bb1bbeb177bbf6eb886af4809e15b1b8ee49a6
Albina Sorokina <albinawolf@gmail.ru>
2021-01-18 17:43:43 +0700

    Linking to button 'Calories'

--- prog/app/src/main/java/com/sorokina/diplom/MainActivity.java
+++ prog/app/src/main/java/com/sorokina/diplom/MainActivity.java
@@ -5,13 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button calor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calor = (Button) findViewById(R.id.calories);
+        calor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCalories();
+            }
+        });
     }
-    
+
+    public void openCalories(){
+        Intent intent_calor = new Intent(this, calories.class);
+        startActivity(intent_calor);
+    }
+
 }
